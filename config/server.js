d4bf467ffb027664533c2dab1282ee7afa7acf40
Gosch <dev@gosch.io>
2020-08-13 22:14:51 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6d687099905213bc22835b3db993454b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3472848-3b5d-43c5-b9c0-5e46db628b83'
+};
